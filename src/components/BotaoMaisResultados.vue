d978d4a4f144462c38ce6e0f804eaf84f8220ba9
alanawessling <wesslingalana@gmail.com>
2026-08-18 11:25:35 -0300

    IF

--- src/components/BotaoMaisResultados.vue
+++ src/components/BotaoMaisResultados.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { ref } from 'vue'
+
+const mais = ref(false)
 </script>    
 <template>
   <div>
-    <button class="MaisResultados">
+    <button class="MaisResultados" @click="mais = !mais">
       <span>Mais Resultados</span>
     </button>
+    <div v-if="mais" class="atividades">
+Colocar atividades
+    </div>
   </div>
 </template>
 
