856407c14d0baf218b2270aed4d5f8a3b90a4748
alanawessling <wesslingalana@gmail.com>
2026-08-18 11:22:28 -0300

    botao simples

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import TheFooter from './components/TheFooter.vue';
+import BotaoMaisResultados from './components/BotaoMaisResultados.vue';
 </script>
 
 <template>
   <div>
     <RouterView/>
     <TheFooter/>
+    <BotaoMaisResultados/>
   </div>
 </template>
 
---
+++ src/components/BotaoMaisResultados.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>    
+<template>
+  <div>
+    <button class="MaisResultados">
+      <span>Mais Resultados</span>
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
