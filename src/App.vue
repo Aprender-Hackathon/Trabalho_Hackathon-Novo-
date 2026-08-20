25538781874a0d13d0fe4dadc4f1692dda19b6ad
PaolaRozanski <paolapriscilarozanski@gmail.com>
2026-08-20 10:38:49 -0300

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import TheFooter from './components/TheFooter.vue';
+import TheHeader from './components/TheHeader.vue';
 </script>
 
 <template>
   <div>
+    <TheHeader/>
     <div class="conteudo">
       <RouterView/>
     </div>
