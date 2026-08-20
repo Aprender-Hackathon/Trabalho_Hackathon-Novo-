51d93518b3ea3d0be582d504b903a7206ad785c9
LarissaBiff <bifflarissa@gmail.com>
2026-08-20 11:17:10 -0300

    Criação da página de teste pra importar os dados pra ficar mais fácil de modelar o componente VerAtv.vue; Exclusão do v-for dessa mesma página, pra não ficar repetindo.

--- src/components/VerAtv.vue
+++ src/components/VerAtv.vue
@@ -1,101 +1,72 @@
 <script setup>
-import { ref } from 'vue'
+import BaixarPdf from './BaixarPdf.vue';
 
-const atividades = ref([
-
-    { 
-        id: 1, 
-        nome_atv: "Dominó", 
-        materia: "ciências", 
-        conteudo: "rochas",
-        descricao: "Atividade de dominó para aprender sobre os diferentes tipos de rochas e suas características.",
-        salvamento: true, 
-        image: '/public/images/atividade_de_alfabetizacao.png'
-    },
-
-    { 
-        id: 2, 
-        nome_atv: "Cartas", 
-        materia: "geografia", 
-        conteudo: "lua",
-        descricao: "Jogo de cartas para conhecer as fases da Lua e compreender suas principais características.",
-        salvamento: false, 
-        image: '/public/images/atividade_de_alfabetizacao.png'
-    },
-
-    { 
-        id: 3, 
-        nome_atv: "Caça-palavras", 
-        materia: "história", 
-        conteudo: "iluminismo",
-        descricao: "Caça-palavras com conceitos e personagens importantes relacionados ao Iluminismo.",
-        salvamento: true, 
-        image: '/public/images/atividade_de_alfabetizacao.png'
-    },
-
-    { 
-        id: 4, 
-        nome_atv: "Quebra-cabeça", 
-        materia: "matemática", 
-        conteudo: "frações",
-        descricao: "Quebra-cabeça educativo para praticar a identificação e a representação de frações.",
-        salvamento: false, 
-        image: '/public/images/atividade_de_alfabetizacao.png'
-    },
-
-    { 
-        id: 5, 
-        nome_atv: "Jogo da Memória", 
-        materia: "português", 
-        conteudo: "sinônimos",
-        descricao: "Jogo da memória para relacionar palavras que possuem significados semelhantes.",
-        salvamento: true, 
-        image: '/public/images/atividade_de_alfabetizacao.png'
-    },
-
-    { 
-        id: 6, 
-        nome_atv: "Palavras Cruzadas", 
-        materia: "inglês", 
-        conteudo: "animais",
-        descricao: "Palavras cruzadas para aprender e praticar o vocabulário de animais em inglês.",
-        salvamento: false, 
-        image: '/public/images/atividade_de_alfabetizacao.png'
-    }
-
-])
+defineProps ({
+    materia: String,
+    titulo: String,
+    conteudo: String,
+    imagem: String,
+    descricao: String
+})
 </script>
 
 <template>
-    <div v-for="atividade in atividades" :key="atividade.id">
+    <div class="all">
         <img
-        :src="atividade.image"
-        :alt="atividade.nome_atv"
+        :src="imagem"
+        :alt="titulo"
         class="image"
       >
+        <div class="lado">
+
         <h3>
-            {{ atividade.nome_atv }}
+            {{titulo || 'Título'}}
         </h3>
 
-        <p>
-            {{ atividade.materia }}
-        </p>
-        <p>
-            {{ atividade.conteudo }}
-        </p>
+        <div class="mat_con">
+
+            <p>
+                {{ materia || 'Matemática'}}
+            </p>
+            <p>
+                {{ conteudo || 'Frações'}}
+            </p>
+
+        </div>
 
         <p>
-            {{ atividade.descricao }}
+            {{ descricao || 'Quebra-cabeça educativo para praticar a identificação e a representação de frações.'}}
         </p>
+
+        <BaixarPdf/>
+
+        </div>
     </div>
 </template>
 
 <style scoped>
-.estrela {
-    color: #ccc;
+.all{
+    display: flex;
+    justify-content: baseline;
+    align-items: center;
+    padding: 5vw;
 }
-
-.estrela.ativa {
-    color: #ffd700;
+.mat_con {
+    display: flex;
+    justify-content: baseline;
+    align-items: center;
+    gap: 15px;
+}
+.mat_con p{
+    background-color: #D1495B;
+    color: #FFFBF6;
+    padding: 5px 15px;
+    border-radius: 100px;
+    font-family: 'Bpmf Huninn';
+}
+.image{
+    width: 100%;
+    max-width: 300px;
+    height: auto;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup>
-import BaixarPdf from '@/components/BaixarPdf.vue';
 import VerAtv from '@/components/VerAtv.vue';
 </script>
 
 <template>
   <main>
     <VerAtv/>
-    <BaixarPdf/>
   </main>
 </template>
 
