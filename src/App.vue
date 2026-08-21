ddbdec5d9c4bf473814c75f7c50af5777a098ad5
alanawessling <wesslingalana@gmail.com>
2026-08-21 12:57:04 -0300

    nada

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+
 import { RouterView } from 'vue-router';
 import TheFooter from './components/TheFooter.vue';
 import BotaoMaisResultados from './components/BotaoMaisResultados.vue';
+
 </script>
 
 <template>
