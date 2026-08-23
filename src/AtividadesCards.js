7faca24dee29c787b55399f8c9cac6171574da82
alanawessling <wesslingalana@gmail.com>
2026-08-23 20:43:55 -0300

    QUASE perdi tudo oq eu tinha feito na AtividadesCards.js

--- src/AtividadesCards.js
+++ src/AtividadesCards.js
@@ -358,5 +358,399 @@ export const atividades = [
 
 //#endregion
 
-//Português
+//#region PORTUGUÊS
+
+//#region ALFABETIZAÇÃO
+
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_A.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_B.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_C.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_D.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_E.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_F.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_G.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_H.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_I.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_J.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_K.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_L.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_M.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_N.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_O.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_P.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_Q.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_R.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_S.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_T.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_U.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_V.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_W.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_X.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  }, 
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_Y.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },  
+  {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Portugues/Alfabetizacao/Atividade_Z.docx',
+    materia: 'Português',
+    conteudo: 'Alfabetização',
+    descricao: 'pipipipopopo'
+  },
+
+//#endregion
+
+//#endregion
+
+//#region Química
+
+//#region Química Organica
+
+ {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica1.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica2.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica3.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica4.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica5.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica6.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica7.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica8.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica9.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica10.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica11.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/SemImagem.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica12.docx',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+   {
+    titulo: 'Adição',
+    imagem: '/atividades/Quimica/Quimica_organica/atividadeQuimica13.png',
+    arquivo: '/atividades/Quimica/Quimica_organica/atividadeQuimica13.pdf',
+    materia: 'Química',
+    conteudo: 'Química orgânica',
+    descricao: 'pipipipopopo'
+  },
+
+
+//#endregion
+
+//#region ÁTOMO E MODELOS ATÔMICOS
+
+   {
+    titulo: 'Adição',
+    imagem: '/atividades/Quimica/Atomo_e_modelos_atomicos/Atomo1.png',
+    arquivo: '/atividades/Quimica/Atomo_e_modelos_atomicos/Atomo1.pdf',
+    materia: 'Química',
+    conteudo: 'Átomo e modelos atômicos',
+    descricao: 'pipipipopopo'
+  },
+     {
+    titulo: 'Adição',
+    imagem: 'SemImagem.png',
+    arquivo: '/atividades/Quimica/Atomo_e_modelos_atomicos/Atomo2.docx',
+    materia: 'Química',
+    conteudo: 'Átomo e modelos atômicos',
+    descricao: 'pipipipopopo'
+  },
+         {
+    titulo: 'Adição',
+    imagem: 'SemImagem.png',
+    arquivo: '/atividades/Quimica/Atomo_e_modelos_atomicos/Atomo3.docx',
+    materia: 'Química',
+    conteudo: 'Átomo e modelos atômicos',
+    descricao: 'pipipipopopo'
+  },
+       {
+    titulo: 'Adição',
+    imagem: 'SemImagem.png',
+    arquivo: '/atividades/Quimica/Atomo_e_modelos_atomicos/Atomo4.docx',
+    materia: 'Química',
+    conteudo: 'Átomo e modelos atômicos',
+    descricao: 'pipipipopopo'
+  },
+         {
+    titulo: 'Adição',
+    imagem: 'SemImagem.png',
+    arquivo: '/atividades/Quimica/Atomo_e_modelos_atomicos/Atomo5.docx',
+    materia: 'Química',
+    conteudo: 'Átomo e modelos atômicos',
+    descricao: 'pipipipopopo'
+  },
+         {
+    titulo: 'Adição',
+    imagem: 'SemImagem.png',
+    arquivo: '/atividades/Quimica/Atomo_e_modelos_atomicos/Atomo6.docx',
+    materia: 'Química',
+    conteudo: 'Átomo e modelos atômicos',
+    descricao: 'pipipipopopo'
+  },
+
+//#endregion
+
+//#region ISOMERIA
+
+         {
+    titulo: 'Adição',
+    imagem: 'SemImagem.png',
+    arquivo: '/atividades/Quimica/Atomo_e_modelos_atomicos/Atomo6.docx',
+    materia: 'Química',
+    conteudo: 'Átomo e modelos atômicos',
+    descricao: 'pipipipopopo'
+  },
+
+//#endregion
+
 ]
