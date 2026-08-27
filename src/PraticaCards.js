388f816829f8dde9659d9a753b3b7768cdd57b66
AnnaClaraMorettiPavanello <annaclaramorettipavanello@gmail.com>
2026-08-27 10:42:08 -0300

    adição das atividades de carnaval e de ano novo

--- src/PraticaCards.js
+++ src/PraticaCards.js
@@ -2,8 +2,7 @@ import { reactive } from 'vue'
 
 export const estadoPratica = reactive({
   lista: [
-
-// ANO NOVO
+    // ANO NOVO
     {
       id: 1,
       titulo: 'Atividade',
@@ -12,7 +11,81 @@ export const estadoPratica = reactive({
       data: 'Ano Novo',
       descricao: 'Atividade temática para celebração de Ano Novo na educação infantil.',
       salvo: false,
-    }
+    },
+    {
+      id: 2,
+      titulo: 'Complete o Desenho',
+      imagem: '/Imagens feriados/Ano Novo/AnoNovo_2.jpg',
+      arquivo: '',
+      data: 'Ano Novo',
+      descricao: 'Atividade de coordenação motora e escrita para o Ano Novo.',
+      salvo: false,
+    },
+    {
+      id: 3,
+      titulo: 'Feliz Ano Novo',
+      imagem: '/Imagens feriados/Ano Novo/carnaval1.jpg',
+      arquivo: '',
+      data: 'Ano Novo',
+      descricao: 'Desenho e atividade comemorativa de Feliz Ano Novo.',
+      salvo: false,
+    },
+
+    // CARNAVAL
+    {
+      id: 4,
+      titulo: 'Carnaval Ensino Fundamental 1',
+      imagem: '/Imagens feriados/Carnaval/carnaval2.jpg',
+      arquivo: '',
+      data: 'Carnaval',
+      descricao: 'Atividades pedagógicas lúdicas sobre o Carnaval.',
+      salvo: false,
+    },
+    {
+      id: 5,
+      titulo: 'Atividades para o Carnaval 2022',
+      imagem: '/Imagens feriados/Carnaval/atividades-para-o-carnaval-2022-5-724x1024.png',
+      arquivo: '',
+      data: 'Carnaval',
+      descricao: 'Exercícios práticos e coloridos de Carnaval.',
+      salvo: false,
+    },
+    {
+      id: 6,
+      titulo: 'Festa de Carnaval',
+      imagem: '/Imagens feriados/Carnaval/carnaval.png',
+      arquivo: '',
+      data: 'Carnaval',
+      descricao: 'Ilustrações e atividades artísticas para o Carnaval.',
+      salvo: false,
+    },
+    {
+      id: 7,
+      titulo: 'Máscaras de Carnaval',
+      imagem: '/Imagens feriados/Carnaval/carnaval3.jpg',
+      arquivo: '',
+      data: 'Carnaval',
+      descricao: 'Pintura e confecção de máscaras carnavalescas.',
+      salvo: false,
+    },
+    {
+      id: 8,
+      titulo: 'Músicas',
+      imagem: '/Imagens feriados/Carnaval/carnaval4.jfif',
+      arquivo: '',
+      data: 'Carnaval',
+      descricao: 'Atividade de exploração cultural e ritmos de Carnaval.',
+      salvo: false,
+    },
+    {
+      id: 9,
+      titulo: 'Cores do Carnaval',
+      imagem: '/Imagens feriados/Carnaval/carnaval5.jfif',
+      arquivo: '',
+      data: 'Carnaval',
+      descricao: 'Exercício prático de pintura e colagem para o Carnaval.',
+      salvo: false,
+    },
   ],
 })
 
