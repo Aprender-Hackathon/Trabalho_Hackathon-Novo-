3afb2bb81abb41a2b7397e098740f7e6b4403811
PaolaRozanski <paolapriscilarozanski@gmail.com>
2026-09-15 23:58:38 -0300

    Resolvendo meus erros

--- src/components/BotaoAddAtvd.vue
+++ src/components/BotaoAddAtvd.vue
@@ -1,20 +1,20 @@
 <template>
-  <router-link to="/outra-pagina" class="botao">+</router-link>
+  <router-link to="/FormularioView.vue" class="botao">+</router-link>
 </template>
 
 <style>
 .botao {
-  display: flex; 
-  align-items: center; 
+  display: flex;
+  align-items: center;
   justify-content: center;
-  width: 60px; 
-  height: 80px; 
-  font-size: 36px; 
+  width: 60px;
+  height: 80px;
+  font-size: 36px;
   font-weight: bold;
-  background: #fce4e4; 
-  border: 3px solid #d95368; 
+  background: #fce4e4;
+  border: 3px solid #d95368;
   border-radius: 16px;
-  color: #b8324b; 
+  color: #b8324b;
   text-decoration: none;
 }
 </style>
