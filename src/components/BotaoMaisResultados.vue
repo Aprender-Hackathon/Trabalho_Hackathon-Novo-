b66e3966703956e7a643a93452eca676aaaad530
alanawessling <wesslingalana@gmail.com>
2026-09-15 23:32:46 -0300

    Comentarios e cores das materias

--- src/components/BotaoMaisResultados.vue
+++ src/components/BotaoMaisResultados.vue
@@ -1,12 +1,11 @@
 <script setup>
-
 defineEmits(['carregar'])
-
 </script>    
+
 <template>
   <div>
-    <button class="MaisResultados" @click="$emit('carregar')" >
-      <span>Mais Resultados </span>
+    <button class="MaisResultados" @click="$emit('carregar')">
+      <span>Mais Resultados</span>
       <img src="/seta.png" />
     </button>
   </div>
@@ -14,8 +13,8 @@ defineEmits(['carregar'])
 
 <style scoped>
 .MaisResultados {
-  width: 23%;
-  height: 39px;
+  width: 240px; 
+  height: 42px;
   background: #fffaf5;
   border: 4px solid #d5485a;
   border-radius: 10px;
@@ -25,6 +24,11 @@ defineEmits(['carregar'])
   font-size: 17px;
   font-weight: bold;
   margin: 0 auto;
-  gap: 30px;
+  gap: 15px;
+  cursor: pointer;
+}
+
+.MaisResultados img {
+  width: 12px;
 }
 </style>
